0625287c89885a29e146a134b1cf4f9436631548
Prakash Shelar <prakashshelar1891@gmail.com>
2021-03-11 21:28:02 +0530

    Update CSS

--- client/index.js
+++ client/index.js
@@ -31,9 +31,19 @@ var ul = document.querySelector("ul");
 TaskContract.events.taskCreated().on("data", (event)=>{
         
     let Temptask = event.returnValues;
+
     var li = document.createElement('li');
+
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.id = Temptask[0];
+    checkbox.addEventListener('change', handleCheckEvent)
+    // checkbox.value = data[0];
+    // checkbox.name = "todo[]";
+    li.appendChild(checkbox);
+
     var eventT = "";
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 3; i++) {
         if(i == 2){            
             var ts = new Date(Temptask[i]*1000);
             
@@ -49,18 +59,11 @@ TaskContract.events.taskCreated().on("data", (event)=>{
         else{
             var T = Temptask[i] + " || ";
         }
-        eventT += T;
         li.appendChild(document.createTextNode(T));
+        eventT += T;
         ul.appendChild(li);
     }
     taskCreatedEvent.innerHTML = eventT;
-
-
-    var dBtn = document.createElement("button");
-	dBtn.appendChild(document.createTextNode("Done"));
-	li.appendChild(dBtn);
-	dBtn.addEventListener("click", deleteListItem);
-    
 });
 
 
@@ -89,13 +92,30 @@ const handleSearchTaskChange = (event) => {
     searchElement = event.target.value;
 }
 
+const handleCheckEvent = (event) => {
+    console.log(event.target.checked);
+    const {id, checked} = event.target;
+    console.log({id, checked});
+    TaskContract.methods.toggleDone(id).send({
+        from: "0xDDc37c741a583eEE12dfeC31FD3d15359a558FCC",
+        gas: "3000000",
+        gasPrice: "1000000"
+    })
+    .on("error",
+            function (error, receipt) {
+                console.log({error, receipt});
+            }
+    )
+}
+
 const handleSearchTaskBtn = () => {
     TaskContract.methods.getTask(searchElement).call().then(
         (data)=>{
             console.log("Getter",data);
-            // searchedTask.innerHTML = data[1];
+            
             var eventT = "";
             for (var i = 0; i < 5; i++) {
+                var T;
                 if(i == 2){            
                     var ts = new Date(data[i]*1000);
                     
@@ -106,10 +126,29 @@ const handleSearchTaskBtn = () => {
                     ":"+ts.getMinutes()+
                     ":"+ts.getSeconds();
                     
+                    T = t + " || ";
+                }
+                else if(i == 4 && data[4]!=0){            
+                    var ts = new Date(data[i]*1000);
+                    var t = ts.getDate()+
+                    "/"+(ts.getMonth()+1)+
+                    "/"+ts.getFullYear()+
+                    " "+ts.getHours()+
+                    ":"+ts.getMinutes()+
+                    ":"+ts.getSeconds();
+                    
                     var T = t + " || ";
                 }
+                else if(i == 3){
+                    if(data[3] == true){
+                        var T = "Completed" + " || ";
+                    }
+                    else{
+                        var T = "Not Completed" + " || ";
+                    }
+                }
                 else{
-                    var T = data[i] + " || ";
+                    T = data[i] + " || ";
                 }
                 eventT += T;
             }
@@ -118,6 +157,71 @@ const handleSearchTaskBtn = () => {
     );
 }
 
+// Show all Tasks 
+TaskContract.methods.total_Tasks().call().then(
+    (data1)=>{
+        newData = Number(data1);
+        for(var j =1; j <= newData; j++){
+             
+            TaskContract.methods.getTask(j).call().then(
+                (data)=>{
+                    var li = document.createElement('li');
+
+                    var checkbox = document.createElement('input');
+                    checkbox.type = "checkbox";
+                    checkbox.id=data[0];
+                    if(data[3] == true){
+                        checkbox.checked = "checked";
+                    }
+                    
+                    checkbox.addEventListener('change',handleCheckEvent);
+                    li.appendChild(checkbox);
+
+                    for (var k = 0; k < 5; k++) {
+                        if(k == 2){            
+                            var ts = new Date(data[k]*1000);
+                            var t = ts.getDate()+
+                            "/"+(ts.getMonth()+1)+
+                            "/"+ts.getFullYear()+
+                            " "+ts.getHours()+
+                            ":"+ts.getMinutes()+
+                            ":"+ts.getSeconds();
+                            
+                            var T = t + " || ";
+                        }
+                        else if(k == 4 && data[4]!=0){            
+                            var ts = new Date(data[k]*1000);
+                            var t = ts.getDate()+
+                            "/"+(ts.getMonth()+1)+
+                            "/"+ts.getFullYear()+
+                            " "+ts.getHours()+
+                            ":"+ts.getMinutes()+
+                            ":"+ts.getSeconds();
+                            
+                            var T = t + " || ";
+                        }
+                        else if(k == 3){
+                            if(data[3] == true){
+                                var T = "Completed" + " || ";
+                            }
+                            else{
+                                var T = "Not Completed" + " || ";
+                            }
+                        }
+                        else{
+                            var T = data[k] + " || ";
+                        }
+                        
+                        li.appendChild(document.createTextNode(T));
+                    }
+                    li.id = data[0];
+                    ul.appendChild(li);
+                }
+            );
+        }
+    }
+    
+)
 
 // Add event listeners
 TaskInputElement.addEventListener("change",handleInputChange);
